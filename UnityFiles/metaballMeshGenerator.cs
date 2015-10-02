9e4794abd17d9c2eaad1c058245e2082f9487248
Kovak <kovac1066@gmail.com>
2015-10-02 00:54:25 -0600

    implement generating mesh from C++ data

--- UnityFiles/metaballMeshGenerator.cs
+++ UnityFiles/metaballMeshGenerator.cs
@@ -48,7 +48,17 @@ public class metaballMeshGenerator : MonoBehaviour
     [DllImport("mbUnity")]
     private static extern void deallocLatticeChargeReturn(int latticeID);
     [DllImport("mbUnity")]
-    private static extern void updateCharge(int latticeID);
+    private static extern void updateLattice(int latticeID);
+    [DllImport("mbUnity")]
+    unsafe private static extern float* getLatticeFrameVertices(int latticeID);
+    [DllImport("mbUnity")]
+    unsafe private static extern float* getLatticeFrameNormals(int latticeID);
+    [DllImport("mbUnity")]
+    unsafe private static extern int* getLatticeFrameTriangles(int latticeID);
+    [DllImport("mbUnity")]
+    private static extern int getLatticeFrameVertCount(int latticeID);
+    [DllImport("mbUnity")]
+    private static extern int getLatticeFrameTriCount(int latticeID);
 
 #endif
     private int latticeID = -1;
@@ -82,10 +92,20 @@ public class metaballMeshGenerator : MonoBehaviour
 
     void Start()
     {
-        latticeID = setupLattice(64, 64, 64, 1.0f, 1.0f, 1.0f);
+        latticeID = setupLattice(10, 10, 10, 1.0f, 1.0f, 1.0f);
+    }
+
+    void startEngine()
+    {
+        ((MeshFilter)GetComponent("MeshFilter")).mesh = new Mesh();
     }
 
     // Update is called once per frame
+    public Vector3[] vertices, normals;
+    public int[] triangles;
+    private int lastVertexCount = -1;
+    private int lastTriangleCount = -1;
+
     void Update()
     {
         int i;
@@ -93,6 +113,7 @@ public class metaballMeshGenerator : MonoBehaviour
         float charge;
         GameObject nodeObject;
         clearLatticeCharges(latticeID);
+        //Debug.Log(nodeObjects.Count);
         for (i = 0; i < nodeObjects.Count; i++)
         {
             nodeObject = nodeObjects[i];
@@ -100,7 +121,52 @@ public class metaballMeshGenerator : MonoBehaviour
             charge = ((metaballNode)nodeObject.GetComponent(typeof(metaballNode))).charge;
             addChargeToLattice(latticeID, position.x, position.y, position.z, charge);
         }
-        updateCharge(latticeID);
+        updateLattice(latticeID);
+        Mesh mesh = ((MeshFilter)GetComponent("MeshFilter")).mesh;
+        int vertexCount = getLatticeFrameVertCount(latticeID);
+        int triangleCount = getLatticeFrameTriCount(latticeID);
+        if (vertexCount != lastVertexCount)
+        {
+            vertices = new Vector3[vertexCount];
+            normals = new Vector3[vertexCount];
+            lastVertexCount = vertexCount;
+        };
+        if (triangleCount != lastTriangleCount)
+        {
+            triangles = new int[triangleCount];
+            lastTriangleCount = triangleCount;
+        }
+        int actualIndex;
+        unsafe
+        {
+            float* verticesData = getLatticeFrameVertices(latticeID);
+            float* normalsData = getLatticeFrameNormals(latticeID);
+            int* trianglesData = getLatticeFrameTriangles(latticeID);
+            for (i = 0; i < vertexCount; i++)
+            {
+                actualIndex = i * 3;
+                //vertices[i] = new Vector3(verticesData[actualIndex], 
+                //    verticesData[actualIndex+1], verticesData[actualIndex+2]);
+                //normals[i] = new Vector3(normalsData[actualIndex],
+                //    normalsData[actualIndex + 1], normalsData[actualIndex + 2]);
+                vertices[i].x = verticesData[actualIndex];
+                vertices[i].y = verticesData[actualIndex + 1];
+                vertices[i].z = verticesData[actualIndex + 2];
+                normals[i].x = normalsData[actualIndex];
+                normals[i].y = normalsData[actualIndex + 1];
+                normals[i].z = normalsData[actualIndex + 2];
+            };
+            for (i = 0; i < triangleCount; i++)
+            {
+                triangles[i] = trianglesData[i];
+            };
+        };
+        //Debug.Log(vertexCount);
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.normals = normals;
+
     }
 
     void OnDisable()
